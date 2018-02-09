29eac3fd7a62c75b2e4a9d5f7633d0e6f41d92df
youngwoon <lywoon89@gmail.com>
2018-02-09 02:40:47 -0800

    add git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -14,7 +14,7 @@ alias gp='git push origin HEAD'
 # Remove `+` and `-` from start of diff lines; just rely upon color.
 alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
 
-alias gc='git commit'
+alias gc='git commit -v'
 alias gca='git commit -a'
 alias gco='git checkout'
 alias gcb='git copy-branch-name'
@@ -22,3 +22,7 @@ alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
 alias ge='git-edit-new'
+
+# youngwoon
+alias ga='git add -p'
+alias gcm='git commit -m'
