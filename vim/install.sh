7e92dbe6fe379a5944b7ac8e319ca1951afcda32
youngwoon <lywoon89@gmail.com>
2018-04-15 16:50:11 -0700

    add powerline font installation script

--- vim/install.sh
+++ vim/install.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 vim +PlugInstall +qall
+
+echo "  Installing Powerline fonts."
+if test "$(uname)" = "Darwin"
+then
+    # clone
+    git clone https://github.com/powerline/fonts.git --depth=1
+    # install
+    cd fonts
+    ./install.sh
+    # clean-up a bit
+    cd ..
+    rm -rf fonts
+    echo "  For macOS users, iTerm2 users need to set both the Regular font and the Non-ASCII Font in (iTerm > Preferences > Profiles > Text) to use a patched font"
+elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
+then
+    sudo apt-get install fonts-powerline
+fi
