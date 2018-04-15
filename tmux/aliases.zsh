86b25b78cd123b3bf60a0046288f426090e945d1
youngwoon <lywoon89@gmail.com>
2018-04-15 16:49:50 -0700

    add tmux alias

--- tmux/aliases.zsh
+++ tmux/aliases.zsh
@@ -1,3 +1,4 @@
-alias ta='tmux attach -t $1'
+alias ta='function _ta(){ tmux attach -t "$1"; }; _ta'
 alias tl='tmux ls'
-alias tk='tmux kill-session -t $1'
+alias tk='function _tk(){ tmux kill-session -t "$1"; }; _tk'
+alias tn='function _tn(){ tmux new-session -s "$1"; }; _tn'
