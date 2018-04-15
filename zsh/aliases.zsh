f89630083c72bfb0f27004b410b45ca71df029cc
youngwoon <lywoon89@gmail.com>
2018-04-15 16:56:03 -0700

    add ls alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,8 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+# ls
+alias ls='ls --color=auto' # colorize the ls output
+# ls='ls -G' for Mac OS?
+alias ll='ls -al'
