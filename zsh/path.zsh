e914c8dfc980086be02f82547dcc81f91b8333a4
Youngwoon Lee <lywoon89@gmail.com>
2023-04-15 16:02:57 -0700

    add conda initialization

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -15,6 +15,23 @@ eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 # <<< pyenv initialize <<<
 
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('$HOME/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "$HOME/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "$HOME/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="$HOME/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+
 # alias python=/usr/bin/python3
 # alias pip=/usr/bin/pip3
 
